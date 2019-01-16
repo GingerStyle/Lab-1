30b370414140dac0478f886b2e35aa10eecad53b
Myles Voigt <myles.voigt@gmail.com>
2019-01-16 16:40:02 -0600

    first try

--- scratch.py
+++ scratch.py
@@ -2,6 +2,6 @@
 name = input('What is your name? ')
 birth_month = input('What is your birth month? ')
 #printing messages
-print('Hello there' + name)
+print('Hello there ' + name)
 if birth_month.upper == 'JANUARY':
-    print('Happy Birthday')
+    print('Happy Birthday this month')
