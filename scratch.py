500d12ffbaff7ca9ed9ccd74f88f55713eb56da6
Myles Voigt <myles.voigt@gmail.com>
2019-01-20 14:31:49 -0600

    Lab 1 Part 1 Complete

--- scratch.py
+++ scratch.py
@@ -5,3 +5,4 @@ birth_month = input('What is your birth month? ')
 print('Hello there ' + name)
 if birth_month.upper() == "JANUARY":
     print('Happy Birthday this month')
+print('There are ' + str(len(name)) + ' letters in your name')
